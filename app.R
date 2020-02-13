80d7db3f9e21f5ae4e12f3ce4a7f1f57aeb4b34e
Fodil Ihaddaden <ihaddaden.fodeil@gmail.com>
2020-02-13 15:33:53 +0100

    first commit

---
+++ app.R
@@ -0,0 +1,72 @@
+
+
+library(shiny)
+
+ui <- fluidPage(
+    
+    
+    titlePanel(strong("Anom")),
+    
+    
+    tabsetPanel(
+        type = "pills",
+
+        tabPanel(
+            "Data",
+            titlePanel("Chargement du Fichier"),
+            sidebarLayout(sidebarPanel(
+                fileInput(
+                    inputId = "info",
+                    label = "Choisissez votre fichier",
+                    accept = c("Excel", "Excel file", ".xlsx"),
+                    buttonLabel = "Chargez ... "
+                )
+                
+                
+                
+            ),
+                mainPanel(
+                    ##################### main panel
+                    h4("Un aperçu de la table INFO : "),
+                    tableOutput("info_content")
+                ))
+        ), 
+        
+        
+        tabPanel(
+            
+            title = "Anom", 
+            
+            titlePanel("Anomalie Detection")
+            
+            
+            
+            
+            
+        )
+        
+    )
+    
+    
+
+   
+)
+
+server <- shinyServer(function(input, output, session) {
+    
+    data <- reactive({
+        req(input$info) 
+        inFile <- input$info 
+        df <- rio::import(inFile$datapath)
+        return(df)
+    })
+    
+    
+    output$info_content <- renderTable({
+        head(data())
+    })
+    
+
+})
+
+shinyApp(ui = ui, server = server)
